f8bfd525a3e83075fc22a4df0d9e10b6496e02e7
Abdullah <abd00s@users.noreply.github.com>
2014-11-23 17:27:02 -0500

    add schedule controller actions

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,4 +1,7 @@
 class SchedulesController < ApplicationController
+  before_action :set_schedule, only: [:show, :edit, :update, :destroy]
+  before_action :set_project, only: [:show, :edit, :new, :create, :update, :destroy]
+
   def index
   end
 
@@ -6,17 +9,44 @@ class SchedulesController < ApplicationController
   end
 
   def new
+    @schedule = @project.schedule.new
   end
 
   def create
+    @schedule = @project.schedule.new(schedule_params)
+    if @schedule.save
+      redirect_to project_path(@project), notice: 'New Schedule Created'
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+    if @schedule.update_attributes(schedule_params)
+      redirect_to project_path(@project)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @schedule.destroy
+    redirect_to project_path(@project)
+  end
+
+  private 
+  def set_schedule
+    @schedule = Schedule.find(params[:id])
+  end
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def schedule_params
+    params.require(:schedule).permit(:id, :project_id, :start_date, :end_date)
   end
 end
